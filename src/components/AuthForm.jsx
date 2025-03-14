058ff20150ab4dfd68b46a95ea1b8508aa8fba18
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-03-14 13:43:14 +0200

    add signup errors visualization

--- src/components/AuthForm.jsx
+++ src/components/AuthForm.jsx
@@ -1,8 +1,19 @@
-import { Form, Link, useSearchParams } from "react-router-dom";
+import {
+  Form,
+  Link,
+  useActionData,
+  useNavigation,
+  useSearchParams,
+} from "react-router-dom";
 
 import classes from "./AuthForm.module.css";
 
 function AuthForm() {
+  const data = useActionData();
+
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
+
   const [searchParams] = useSearchParams();
   const isLogin = searchParams.get("mode") === "login";
 
@@ -10,19 +21,28 @@ function AuthForm() {
     <>
       <Form method="post" className={classes.form}>
         <h1>{isLogin ? "Log in" : "Create a new user"}</h1>
+        {data && data.message && <p>{data.message}</p>}
         <p>
           <label htmlFor="email">Email</label>
           <input id="email" type="email" name="email" required />
+          {data?.errors?.email && (
+            <span className={classes.error}>{data.errors.email}</span>
+          )}
         </p>
         <p>
           <label htmlFor="image">Password</label>
           <input id="password" type="password" name="password" required />
+          {data?.errors?.password && (
+            <span className={classes.error}>{data.errors.password}</span>
+          )}
         </p>
         <div className={classes.actions}>
           <Link to={`?mode=${isLogin ? "signup" : "login"}`}>
             {isLogin ? "Create new user" : "Login"}
           </Link>
-          <button>Save</button>
+          <button disable={isSubmitting}>
+            {isSubmitting ? "Submitting..." : "Save"}
+          </button>
         </div>
       </Form>
     </>
