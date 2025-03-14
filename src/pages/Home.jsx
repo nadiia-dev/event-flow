079f33b8f16931e90cabfcaee2d7410f6c01283f
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-03-14 14:28:41 +0200

    customized home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import PageContent from "../components/PageContent";
+
 function HomePage() {
-  return <h1>HomePage</h1>;
+  return (
+    <PageContent title={"Welcome!"}>
+      <p>Browse our incredible events</p>
+    </PageContent>
+  );
 }
 
 export default HomePage;
