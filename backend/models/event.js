dce351af83ae1699976b2a74601d0152f3d0a35d
nadiia-dev <nadiia.pavliuchenko.dev@gmail.com>
2025-03-17 17:45:51 +0200

    add mongoose models

---
+++ backend/models/event.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+
+const EventSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    date: { type: String, required: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+  },
+  { versionKey: false }
+);
+
+const Event = mongoose.models.Event || mongoose.model("Event", EventSchema);
+export default Event;
